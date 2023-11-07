ae023f904de2332fddaf8ad51761cd3a495c55dd
Job Moses <jobmoses90@gmail.com>
2023-11-07 04:44:47 +0100

    v1

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*============    typing animation =======*/
+var typing = new Typed(".typing",{
+    strings:["Chemist","Data Analyst","Font- End Web Developer", ],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
