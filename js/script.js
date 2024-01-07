c906b601672bbfef6ea8543b2d851cc8b8fd9176
Job Moses <jobmoses90@gmail.com>
2024-01-07 13:18:01 +0100

    'NV5'

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*============    typing animation =======*/
 var typing = new Typed(".typing",{
-    strings:["Data Analyst","Developer",'Chemist'],
+    strings:["Data Analyst","Web Developer",'Chemist'],
     typeSpeed:100,
     BackSpeed:80,
     loop:true
