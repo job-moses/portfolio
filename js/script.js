cdcdaaa4ff97b012307c84ecf56ef48db5b7602c
Job Moses <jobmoses90@gmail.com>
2024-08-29 19:19:32 +0100

    N12

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*============    typing animation =======*/
 var typing = new Typed(".typing",{
-    strings:["Developer","Data Analyst",'Chemist'],
+    strings:["Web Developer","Data Analyst",'Chemist'],
     typeSpeed:100,
     BackSpeed:80,
     loop:true
