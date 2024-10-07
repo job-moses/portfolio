5482b32fc8c14ccc90b58e309a544e3ab181fa60
Job Moses <jobmoses90@gmail.com>
2024-10-07 14:38:13 +0100

    N19

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ var typing = new Typed(".typing",{
     BackSpeed:80,
     loop:true
 })
+
+var typing = new Typed(".newtyping",{
+    strings:["Web Developer","Data Analyst",'Chemist'],
+    typeSpeed:100,
+    BackSpeed:80,
+    loop:true
+})
